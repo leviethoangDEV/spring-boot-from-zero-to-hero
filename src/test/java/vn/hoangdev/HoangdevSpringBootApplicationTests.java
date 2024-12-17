559d3176d8b92b5f58c03a13c70c0ead5463c041
leviethoang197 <leviethoang.aws@gmail.com>
2024-12-17 20:14:21 +0700

    [feat]: Build structure for project

---
+++ src/test/java/vn/hoangdev/HoangdevSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hoangdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoangdevSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
